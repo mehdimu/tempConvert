2b5f5ab7d9683dd1995789d8fe12ad197e8c6ab7
Mehdi Ali <alimehdi92@hotmail.com>
2014-01-03 19:46:03 -0500

    added feature with a button, button works only

--- src/com/example/tempconvert/MainActivity.java
+++ src/com/example/tempconvert/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.tempconvert;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -11,12 +14,22 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	
+	 public void convert(View view) {
+	     // Do something
+		 
+		 EditText edittext = (EditText) findViewById(R.id.celsius);
+		 String num = edittext.getText().toString();
+		 TextView textview = (TextView) findViewById(R.id.result_text);
+		 textview.setText(num);
+	 
+	 }
+	 
+	//@Override
+	//public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+		//getMenuInflater().inflate(R.menu.main, menu);
+		//return true;
+	//}
 
 }
